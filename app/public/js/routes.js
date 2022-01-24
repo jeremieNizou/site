c0d850321e708ed14ad83a64312bfce21d1d9b3a
Jérémie Nizou <jeremie.nizou@gmail.com>
2022-01-24 22:53:03 +0100

    v1.6..5: Add maintenance

--- app/public/js/routes.js
+++ app/public/js/routes.js
@@ -5,27 +5,31 @@ var myApp = angular.module("myApp", ["ngRoute", "ngCookies", "ngAnimate"]);
 myApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$routeProvider
-		.when("/todolisteBDD", {
-			templateUrl: "views/todolistHome.html",
-			controller: "todolistHomeCtrl"
-		})
-		.when("/todolisteBDD/presentation/:id", {
-			templateUrl: "views/todolistPres.html",
-			controller: "todolistPresCtrl"
-		})
-		.when("/todoliste", {
-			templateUrl: "views/todolist.html",
-			controller: "todolistCtrl"
-		})
-		.when("/lebonthe", {
-			templateUrl: "views/lebonthe.html"
-		})
-		.when("/music", {
-			templateUrl: "views/music.html",
-			controller: "musicCtrl"
-		})
+		// .when("/todolisteBDD", {
+		// 	templateUrl: "views/todolistHome.html",
+		// 	controller: "todolistHomeCtrl"
+		// })
+		// .when("/todolisteBDD/presentation/:id", {
+		// 	templateUrl: "views/todolistPres.html",
+		// 	controller: "todolistPresCtrl"
+		// })
+		// .when("/todoliste", {
+		// 	templateUrl: "views/todolist.html",
+		// 	controller: "todolistCtrl"
+		// })
+		// .when("/lebonthe", {
+		// 	templateUrl: "views/lebonthe.html"
+		// })
+		// .when("/music", {
+		// 	templateUrl: "views/music.html",
+		// 	controller: "musicCtrl"
+		// })
+		// .when("/", {
+		// 	templateUrl: "views/acceuil.html", 
+		// 	controller: "acceuilCtrl"
+		// })
 		.when("/", {
-			templateUrl: "views/acceuil.html", 
+			templateUrl: "views/maintenance.html", 
 			controller: "acceuilCtrl"
 		})
 		.otherwise({
